9caa7303f084643fb3c974e4fc12789f40b3846c
John Vennard <vennard@mumble-23.cs.wisc.edu>
2014-04-02 11:03:26 -0500

    nice working framework complete

--- p3/mem.c
+++ p3/mem.c
@@ -1,18 +1,20 @@
 #include "mem.h"
+#include <stdio.h>
 
 int Mem_Init(int sizeOfRegion) {
    return 7;
 }
 
 void *Mem_Alloc(int size) {
+   printf("success!\r\n");
    return 0;
 }
 
 int Mem_Free(void *ptr) {
-   return 0;
+   return 5;
 }
 
 void Mem_Dump() {
-
+   printf("success!\r\n");
 }
 
--- p3/mem_tester.c
+++ p3/mem_tester.c
@@ -4,6 +4,13 @@
 int main() {
    printf("Starting mem testing...\r\n");
    int result = Mem_Init(32);
-   if (result == 7) printf("Successfully called Mem_Init()\r\n");
+   if (result == 7) printf("Successfully called Mem_Init()!\r\n");
+   printf("Calling Mem_Alloc()... ");
+   Mem_Alloc(32);
+   int *ptr = 0;
+   result = Mem_Free(ptr);
+   if (result == 5) printf("Successfully called Mem_Free()!\r\n");
+   printf("Calling Mem_Dump()... ");
+   Mem_Dump();
    return 0;
 }
