0a9148025b7f471f1ac9bba44f88390405ce20fb
John Vennard <vennard@mumble-11.cs.wisc.edu>
2014-04-09 18:49:55 -0500

    debugging craziest thing ever

--- p3/mem/mem.c
+++ p3/mem/mem.c
@@ -227,7 +227,21 @@ void *Mem_Alloc(int size) {
    addr->next = NULL;
    addr->size = fsize + sizeof(struct header);
    //TODO TEST HERE - try different style write
+   /*struct header test;
+   test.key = KEY;
+   test.next = NULL;
+   test.size = fsize + sizeof(struct header);
+   memcpy(addr,&test,sizeof(struct header));
+*/
+   printf("1: DATA AT ADDR: key: %c, size: %i, next: %p\r\n",addr->key,addr->size,addr->next);
+   printf("2: DATA AT ADDR: key: %c, size: %i, next: %p\r\n",addr->key,addr->size,addr->next);
+   printf("3: DATA AT ADDR: key: %c, size: %i, next: %p\r\n",addr->key,addr->size,addr->next);
+   printf("4: DATA AT ADDR: key: %c, size: %i, next: %p\r\n",addr->key,addr->size,addr->next);
+   //addr = (addr + 1); //TODO for some unknown reason this locks previous reads to constant value messes everything else up obviously
+   printf("5: DATA AT ADDR: key: %c, size: %i, next: %p\r\n",addr->key,addr->size,addr->next);
    printf("------------------------> wrote new block to address %p w/ size %i <-------------------------------\r\n",addr,addr->size);
+   //memcpy(addr,&test,sizeof(struct header));
+   //printf("DATA AT ADDR: key: %c, size: %i, next: %p\r\n",addr->key,addr->size,addr->next);
    int check = insert(addr); //inserts and set next variable in header
    if (check == 0) {
       printf("Insert call was a success!\r\n");
