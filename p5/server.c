2150102f49882547140ca585484c987b7328a505
John Vennard <vennard@mumble-31.cs.wisc.edu>
2014-05-09 18:49:22 -0500

    goddamn dont ever use structs if you really want to know where stuff is

--- p5/server.c
+++ p5/server.c
@@ -47,14 +47,13 @@ typedef struct imap {
 } imap;
 
 typedef struct inode {
-   int size; //number of the last byte in the file?
-   int type; //1 - file, 0 - directory
+   MFS_Stat_t stat;
+   //int size;
+   //int type;
    int data_ptr[14]; //14 direct pointers to data blocks
    //if directory data blocks contain name and inode pairs
    //name is fixed length size 60 bytes
    //inode number is just 4 bytes
-   //every directory has . and .. entries
-   //unused directory entries have inode number = 0
 } inode;
 
 typedef struct datablock {
@@ -79,60 +78,124 @@ void initializefs() {
 
    //create new CR region
    eol = 0;
+   int iptrs[256];
+   iptrs[0] = 1028;
+   lseek(fd, 4, SEEK_SET);
+   write(fd, iptrs, 1024);
+/*
    checkregion cr;
    cr.imap_ptr[0] = sizeof(cr);
    cr.eol = sizeof(cr);
    write(fd, &cr, sizeof(cr));
-   printf("Eol for CR: %i!\r\n",eol);
-
+   printf("CR: %i (%i bytes)\r\n",eol,sizeof(cr));
+   printf("     imap_ptr[0] - %i\r\n",cr.imap_ptr[0]);
+*/
    //initialize inode map
-   eol = sizeof(cr);
+   eol = 1028; 
    imap im;
    for(i = 0;i < 256;i++) im.inode_ptr[i] = 0;
    im.inode_ptr[0] = eol + sizeof(im);
    numinodes = 1;
    lseek(fd, eol, SEEK_SET);
    write(fd, &im, sizeof(im));
-   printf("Eol for imap: %i!\r\n",eol);
+   printf("imap: %i (%i bytes)\r\n",eol,sizeof(im));
+   printf("       inode_ptr[0] - %i\r\n",im.inode_ptr[0]);
 
    //create first inode (root directory)
+   eol += 64;
+   MFS_Stat_t st;
+   st.type = 0;
+   st.size = 4096;
+   int size = 4096;
+   int type = 0;
+   int ptrs[14];
+   ptrs[0] = eol + 64;
+   ptrs[1] = eol + 64 + 64;
+   for (i = 2; i < 14;i++) ptrs[i] = 0;
+   lseek(fd, eol, SEEK_SET);
+   //write(fd, &st,sizeof(st));
+   write(fd, &size, 4);
+   lseek(fd, eol+4, SEEK_SET);
+   write(fd, &type, 4);
+   lseek(fd, eol+8, SEEK_SET);
+   write(fd, ptrs, 56);
+   printf("inode: %i (64 bytes)\r\n",eol);
+   printf("        ptr[0] - %i\r\n",ptrs[0]);
+   printf("        ptr[1] - %i\r\n",ptrs[1]);
+
+   /*
    eol += sizeof(im);
    inode root;
-   for(i = 0;i < 14;i++) root.data_ptr[i] = 0;
-   root.size = 4096;
-   //root.size = eol + sizeof(root);
-   root.type = 0; 
+   for(i = 2;i < 14;i++) root.data_ptr[i] = 0;
+   root.stat.size = 4096;
+   //root.size = 4096;
+   root.stat.type = 0; 
+   //root.type = 0;
    root.data_ptr[0] = eol + sizeof(root);
    root.data_ptr[1] = eol + sizeof(root) + sizeof(MFS_DirEnt_t);
-printf("inode pointers 0: %i and 1: %i!\r\n",root.data_ptr[0],root.data_ptr[1]);
-   
+   //root.data_ptr[2] = sizeof(MFS_DirEnt_t);
+   printf("inode pointers 0: %i and 1: %i!\r\n",root.data_ptr[0],root.data_ptr[1]);
    lseek(fd, eol, SEEK_SET);
    write(fd, &root, sizeof(root));
    printf("Eol for inode: %i!\r\n",eol);
 
+*/
    //create single root directory and populate (with . and ..)
+   eol += 64;
+   int inu = 0;
+   char bf[60];
+   bf[0] = '.';
+   bf[1] = '.';
+   bf[2] = '\0';
+   lseek(fd, eol, SEEK_SET);
+   write(fd, &inu,4);
+   lseek(fd, eol+4, SEEK_SET);
+   write(fd, bf,60);
+/*
    MFS_DirEnt_t dr1;
-   sprintf(dr1.name,".");
    dr1.inum = 0; //roots inode number
-   eol += sizeof(root);
+   //sprintf(dr1.name,"SOMETHINGS HAS TO HAPPEN-----------------------");
+   sprintf(dr1.name,".");
    lseek(fd, eol, SEEK_SET);
    write(fd, &dr1, sizeof(dr1));
-   printf("Eol for dr1: %i (Entry %s with num %i)!\r\n",eol,dr1.name,dr1.inum);
+*/
+   printf("dirent 1: %i (%i bytes)\r\n",eol, 64);
 
    MFS_DirEnt_t dr2;
-   eol += sizeof(dr1);
+   eol += sizeof(dr2);
+   //sprintf(dr2.name,".................................\0");
    sprintf(dr2.name,"..");
-   dr2.inum = dr1.inum; //can't go past this
+   dr2.inum = 0; //can't go past this
    lseek(fd, eol, SEEK_SET);
    write(fd, &dr2, sizeof(dr2));
-   printf("Eol for dr2: %i (Entry %s with num %i)!\r\n",eol,dr2.name,dr2.inum);
+   printf("dirent 2: %i (%i bytes)\r\n",eol, sizeof(dr2));
 
    eol += sizeof(dr2);
-   cr.eol = eol;
    lseek(fd, 0, SEEK_SET); 
-   write(fd, &cr, sizeof(cr)); 
+   write(fd, &eol, 4); 
    printf("done writing everything out");   
    printf("final eol: %i!\r\n",eol);
+
+
+
+   printf("------------read back check----------\r\n");
+   lseek(fd, 4, SEEK_SET);
+   int cimap, cinode, csize, ctype, cd1, cd2;
+   read(fd, &cimap, 4);
+   printf("check region ptr to imap gave: %i\r\n",cimap);
+   lseek(fd, cimap, SEEK_SET);
+   read(fd, &cinode, 4);
+   printf("imap ptr to inode gave: %i\r\n",cinode);
+   lseek(fd, cinode, SEEK_SET);
+   read(fd, &csize, 4);
+   lseek(fd, cinode+4, SEEK_SET);
+   read(fd, &ctype, 4);
+   lseek(fd, cinode+8, SEEK_SET);
+   read(fd, &cd1, 4);
+   printf("inode - size: %i type: %i\r\n",csize,ctype);
+   printf("        ptr to direntry 1: %i \r\n",cd1);
+   
+  
 
    fsync(fd);
 }
